fc2790484ad15d7f223546a5e88c2ccf8eaeab2e
SrujanDeshmukh <srujandeshmukh9@gmail.com>
2026-04-15 12:04:52 +0530

    user module + entities added

--- Backend/hotelordering/src/main/java/com/example/hotelordering/entity/OrderItem.java
+++ Backend/hotelordering/src/main/java/com/example/hotelordering/entity/OrderItem.java
@@ -1,4 +1,30 @@
-package com.example.hotelordering.entity;
+package com.hcl.foodapp.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "order_items")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "menu_item_id")
+    private MenuItem menuItem;
+
+    private Integer quantity;
+
+    private Double price;
 }
